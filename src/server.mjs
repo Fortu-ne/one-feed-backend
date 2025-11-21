9a65514c05b73c9ac0cc865d26ac1cb077e045cf
Fortu-ne <fortunet711@outlook.com>
2025-11-21 19:15:29 +0200

    Project Structure Config

---
+++ src/server.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req,res) =>{
+
+	return res.send({msg: "Welcome to the home page"});
+});
